882ee61daaaea3e660f96106e0e3f49f84fa7ce6
Yuganshu <yuganshujoshi1040@gmail.com>
2024-07-31 08:46:36 +0530

    added Reachable Nodes with Restrictions

---
+++ DSA/Graph/Disjoint Set Union/Reachable Nodes with Restrictions.cpp
@@ -0,0 +1,88 @@
+/*  Leetcode link : https://leetcode.com/problems/reachable-nodes-with-restrictions/
+    Difficulty: Medium
+*/
+
+#define inc()                                                                  \
+    ios_base::sync_with_stdio(0);                                              \
+    cin.tie(0);                                                                \
+    cout.tie(0)
+
+class DSU {
+public:
+    vector<int> parent, rank, size;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n);
+        size.resize(n);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == parent[x])
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void unionByRank(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent)
+            return;
+
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    void unionBySize(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent || !size[x_parent] || !size[y_parent])
+            return;
+
+        if (size[x_parent] > size[y_parent]) {
+            parent[y_parent] = x_parent;
+            size[x_parent] += size[y_parent];
+        } else {
+            parent[x_parent] = y_parent;
+            size[y_parent] += size[x_parent];
+        }
+    }
+};
+
+class Solution {
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        inc();
+
+        DSU dsu(n);
+
+        for (int& resNode : restricted) {
+            dsu.size[resNode] = 0;
+        }
+
+        for (auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (dsu.find(u) != dsu.find(v))
+                dsu.unionBySize(u, v);
+        }
+
+        return dsu.size[dsu.find(0)];
+    }
+};
