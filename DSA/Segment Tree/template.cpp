05d932a078d53bfbafdc32b64eb7caf5541161cc
Yuganshu <yuganshujoshi1040@gmail.com>
2025-07-28 18:01:28 +0530

    removed array in updateTree() and query()

--- DSA/Segment Tree/template.cpp
+++ DSA/Segment Tree/template.cpp
@@ -21,7 +21,7 @@ class segTree {
         sTree[i] = sTree[2 * i + 1] + sTree[2 * i + 2];
     }
     
-    void updateTree(int arr[], int idx, int i, int l, int r, int val) {
+    void updateTree(int idx, int i, int l, int r, int val) {
         if (l == r) {
             sTree[i] = val;
             return;
@@ -30,15 +30,15 @@ class segTree {
         int mid = l + (r - l) / 2;
         
         if (idx <= mid) {
-            updateTree(arr, idx, 2 * i + 1, l, mid, val);
+            updateTree(idx, 2 * i + 1, l, mid, val);
         } else {
-            updateTree(arr, idx, 2 * i + 2, mid + 1, r, val);
+            updateTree(idx, 2 * i + 2, mid + 1, r, val);
         }
         
         sTree[i] = sTree[2 * i + 1] + sTree[2 * i + 2];
     }
     
-    int query(int arr[], int l, int r, int start, int end, int i) {
+    int query(int l, int r, int start, int end, int i) {
         if (start > r || end < l)
             return 0;
         
@@ -47,7 +47,7 @@ class segTree {
         }
         
         int mid = l + (r - l ) / 2;
-        return query(arr, l, mid, start, end, 2 * i + 1) +
-                query(arr, mid + 1, r, start, end, 2 * i + 2);
+        return query(l, mid, start, end, 2 * i + 1) +
+                query(mid + 1, r, start, end, 2 * i + 2);
     }
 };
